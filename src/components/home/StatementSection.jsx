b9184c92c5a45577596fa0389dc44fc520b231c3
Nour <kkid607@gmail.com>
2026-06-29 15:30:56 +0300

    statement section modified

--- src/components/home/StatementSection.jsx
+++ src/components/home/StatementSection.jsx
@@ -4,7 +4,7 @@ const NewStatementSection = () => {
   return (
     <div className="container  overflow-visible">
       <div className="grid grid-cols-1  lg:grid-cols-3 items-center justify-items-center lg:justify-items-normal lg:gap-50 xl:gap-0 relative  rounded-[50px] bg-linear-to-br from-[#3E78C5] to-[#1E3A5F]">
-        <div className=" lg:relative w-80 lg:w-108 xl:w-auto">
+        <div className=" lg:relative w-80 lg:w-108 pt-2 xl:w-auto">
           <Image
             src="/ceo.png"
             alt="ceo"
@@ -20,7 +20,7 @@ const NewStatementSection = () => {
             alt="quote mark"
             width={64}
             height={64}
-            className="absolute hidden lg:block top-4 lg:-right-18 xl:-right-6"
+            className="absolute w-10 lg:w-16 lg:block top-2 lg:top-4 right-6 lg:-right-18 xl:-right-6"
           />
           <h1 className="text-2xl md:text-3xl font-semibold mb-8 w-full text-center lg:text-start">
             كلمة رئيس المركز
@@ -34,7 +34,7 @@ const NewStatementSection = () => {
             </h1>
             <div className="hidden lg:block absolute inset-s-0 top-1/2 -translate-y-1/2 h-40 w-1 bg-[#EC4D38]" />
           </div>
-          <div className="flex justify-end px-2 xl:px-0 lg:justify-start items-center gap-4 lg:gap-36 mt-4 w-full">
+          <div className="flex justify-evenly text-center md:text-start px-2 xl:px-0 lg:justify-start items-center gap-4 lg:gap-36 mt-4 w-full">
             <div className="">
               <h2 className="text-lg md:text-2xl font-bold">
                 د.عبد المنعم السيد
@@ -48,7 +48,7 @@ const NewStatementSection = () => {
               alt="quote mark"
               width={64}
               height={64}
-              className="w-10 h-10 hidden lg:block lg:w-16 lg:h-16"
+              className="w-10 h-10  lg:block lg:w-16 lg:h-16"
             />
           </div>
         </div>
